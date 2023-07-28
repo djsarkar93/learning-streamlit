4f295fb05b7940cd45c184adf7e60c2b4981eb88
Dibyajyoti Sarkar <46948111+djsarkar93@users.noreply.github.com>
2023-07-28 19:54:52 +0530

    Learn write command

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
 
 if __name__ == '__main__':
   st.title(f'My First Streamlit App!')
+
   
   # Working with media
   st.header('Working with media')
   
-  st.subheader('Images')
+  st.subheader('Image')
   st.markdown('API Reference: [here](https://docs.streamlit.io/library/api-reference/media/st.image)')
   st.image('resources/image.jpg', width = 420)
 
@@ -17,3 +21,19 @@ if __name__ == '__main__':
   st.subheader('Video')
   st.markdown('API Reference: [here](https://docs.streamlit.io/library/api-reference/media/st.video)')
   st.video('resources/video.mp4')
+
+  
+  # Working with text
+  st.header('Working with text')
+
+  st.subheader('Write')
+  st.markdown('API Reference: [here](https://docs.streamlit.io/library/api-reference/write-magic/st.write)')
+  st.write(1234)
+  st.write('1 + 1 = ', 2)
+  st.write('Hello, *World!* :sunglasses:')
+  df_1 = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, np.nan, np.inf]})
+  st.write(df_1)
+  df_2 = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+  c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+  st.write(c)
+  
